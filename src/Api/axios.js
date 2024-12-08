d18629e482a0f5b5ee17c9c2b997003565e1e5c9
Feven Issayas <fevennissayas@gmail.com>
2024-12-08 22:19:44 +0300

    baseurl

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-a881a/us-central1/api",
+  baseURL: "https://amazon-api-deploy-la5u.onrender.com",
+  // baseURL: "http://127.0.0.1:5001/clone-a881a/us-central1/api",
 });
 
 export {axiosInstance}
